833d2a3f8b4008001bfac7a4fea593ce60385b51
Milo <zoomzang@gmail.com>
2022-05-21 13:42:19 -0700

    Changed to WinForm .NET Framework 4.7.2
    I was on a console app, but now I must build out my GUI.

---
+++ Pachinkasu/Pachinkasu/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pachinkasu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            PlayPanel plaPa = new PlayPanel(5, 2, new Random());
+            plaPa.SwapPin(3, 0);
+            plaPa.SwapPin(0, 0); // saying it twice would remove it all together thrice would be once
+            plaPa.SwapPin(2, 1);
+            plaPa.SwapPin(4, 1);
+            plaPa.DropEveryLocation();
+            int[] results = plaPa.GetResult();
+            for (int i = 0; i < 5; i++)
+            {
+                MessageBox.Show(results[i] + "");
+            }
+        }
+    }
+}
